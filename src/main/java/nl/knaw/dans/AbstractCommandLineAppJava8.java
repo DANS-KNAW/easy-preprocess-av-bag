a1a7638b30c36d6f5df777cdf932cda03643a786
jo-pol <jo-pol@users.noreply.github.com>
2024-08-01 10:42:05 +0200

    Revert reversion (changes didn´t cause the problem)
    This reverts commit 03e2acc593916318e3f82d39420affb8553f7cbb.
    
    Just changing a blank line also had the same problem
    See https://github.com/DANS-KNAW/easy-preprocess-av-bag/pull/5

--- src/main/java/nl/knaw/dans/AbstractCommandLineAppJava8.java
+++ src/main/java/nl/knaw/dans/AbstractCommandLineAppJava8.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.knaw.dans; // TODO lib/util
+package nl.knaw.dans;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
